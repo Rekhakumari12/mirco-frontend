b9c3f05a79f199b0b48a2487f18ef69bb46f3079
Rekha Kumari <rekha.kumari@MACPRO181.local>
2022-12-08 15:09:29 +0530

    done imageloader

---
+++ 5. Image_Loader/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "images/[hash][ext]",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css)$/, //css part that need to match at the end of the particular file, $ specifies it has to at the end of the file
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader", options: { modules: true } }, //option property is require to enable css modules
+        ],
+      },
+      {
+        test: /.s[ac]ss$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader", options: { modules: true } }, //option property is require to enable css modules
+          {loader: "sass-loader" } //deal with scss import statements 
+        ],
+      },
+      {
+        test: /.(png|jpeg|gif|svg)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
