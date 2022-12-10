4b37e86dfeff647f2677d668c410838ca5d67588
Rekha Kumari <rekha.kumari@MACPRO181.local>
2022-12-10 20:42:29 +0530

    add babel loaders

--- with-react/src/App.js
+++ with-react/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Login from './Pages/Login/login.jsx';
+import Movies from './Pages/Movies/Movies.jsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/movies" component={Movies} />
+        <Route path="/">
+          <Redirect to="/login" />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- with-react/webpack.config.js
+++ with-react/webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
       template: "./src/index.html",
       filename: "index.html",
     }),
+    new miniCssExtractPlugin(),
   ],
   devServer: {
     static: {
@@ -26,20 +27,46 @@ module.exports = {
     port: 9000,
     open: true, //like we added in package.json
   },
-  module:{
+  module: {
     rules: [
       {
+        test: /\.(js|jsx)$/,
+        include: path.resolve(__dirname, "src"),
+        exclude: path.resolve(__dirname, "node_modules"),
+        use: [
+          {
+            loader: "babel-loader", //added babel loader to make webpack understand JXS
+            options: {
+              presets: [ //required for babel loader configurations
+                [
+                  "@babel/preset-env",
+                  {
+                    targets: "defaults",
+                  },
+                ],
+                "@babel/preset-react",
+              ],
+            },
+          },
+        ]
+      },
+        {
           test: /\.css$/,
           use: [miniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
+        },
+        {
           test: /\.s[ac]ss$/,
           use: [miniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(png|jpeg|jpg|gif)$/,
-        type: "asset/resource", //in-built loader
+        },
+        {
+          test: /\.(png|jpeg|jpg|gif)$/,
+          type: "asset/resource", //in-built loader
+        },
+    ],
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
     },
-  ],
-  }
+  },
 }
