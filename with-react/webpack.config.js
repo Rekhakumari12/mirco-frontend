2caee5bcbd7da47d09b37d7ee3bafd56a47c874d
Rekha Kumari <rekha.kumari@MACPRO181.local>
2022-12-10 20:27:25 +0530

    add loaders

--- with-react/webpack.config.js
+++ with-react/webpack.config.js
@@ -1,7 +1,13 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const miniCssExtractPlugin = require("mini-css-extract-plugin");
+const EsLintPlugin = require("eslint-webpack-plugin");
+
+
+
 
 module.exports = {
+  mode: "development",
   entry: path.resolve(__dirname, "src/index.js"),
   output: {
     filename: "[name].bundle.js",
@@ -13,4 +19,27 @@ module.exports = {
       filename: "index.html",
     }),
   ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 9000,
+    open: true, //like we added in package.json
+  },
+  module:{
+    rules: [
+      {
+          test: /\.css$/,
+          use: [miniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+          test: /\.s[ac]ss$/,
+          use: [miniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jpeg|jpg|gif)$/,
+        type: "asset/resource", //in-built loader
+    },
+  ],
+  }
 }
